493d34a01ad84e2a853ef407515a9a255c82b0ac
Sharmankin <artemalex1107@gmail.com>
2023-11-10 00:37:38 +0300

    printSecondMassage

--- src/test/java/HelloWordTest.java
+++ src/test/java/HelloWordTest.java
@@ -1,15 +1,32 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 
 public class HelloWordTest {
 
     @Test
-    public void testHelloWord(){
-       Response response = RestAssured
-               .get("https://playground.learnqa.ru/api/hello")
-               .andReturn();
-       response.prettyPrint();    }
+    public void testHelloWord() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.print();
+    }
+
+
+    @Test
+    public void printSecondMassage() {
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        List<String> messages = response.getList("messages.message");
+        String message2 = messages.get(1);
+        System.out.println(message2);
+    }
+
 }
-
-
